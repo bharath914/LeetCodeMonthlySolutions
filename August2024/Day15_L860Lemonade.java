e8c96f72fe3af1e167aeb6e3051f575412e55198
Bharath <prakash.ayinala7@gmail.com>
2024-08-15 12:02:58 +0530

    Added Example

--- August2024/Day15_L860Lemonade.java
+++ August2024/Day15_L860Lemonade.java
@@ -1,26 +1,29 @@
 public class Day15_L860Lemonade {
     public static void main(String[] args) {
-
+        L860Sol sol = new L860Sol();
+        boolean possible = sol.lemonadeChange(new int[]{5,5,5,10,20});
     }
 }
-class L860Sol{
+
+class L860Sol {
     public boolean lemonadeChange(int[] bills) {
         int fives = 0;
         int tens = 0;
-        for(int ele : bills){
+        for (int ele : bills) {
 
-            if(ele==5)fives++;
-            else if(ele ==10){
+            if (ele == 5) fives++;
+            else if (ele == 10) {
                 tens++;
-                if(fives==0)return false;
+                if (fives == 0) return false;
                 fives--;
-            }
-            else {
-                if(fives==0)return false;
-                if(tens==0){
-                    if(fives<3)return false;
-                    fives-=3;
-                }else{tens--;fives--;
+            } else {
+                if (fives == 0) return false;
+                if (tens == 0) {
+                    if (fives < 3) return false;
+                    fives -= 3;
+                } else {
+                    tens--;
+                    fives--;
                 }
             }
         }
